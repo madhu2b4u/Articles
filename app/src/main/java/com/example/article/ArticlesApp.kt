fa070f0811db5eab66994e84a3ff4747e4eec9f2
M MADHU KALYAN <raydriiger@gmail.com>
2019-12-13 17:00:47 +0300

    Test driven development Unit tests -Remote data source test check success and error state -repository test checks exceptions and exception -view-model and use case test check error, loading and success state

---
+++ app/src/main/java/com/example/article/ArticlesApp.kt
@@ -0,0 +1,21 @@
+package com.example.article
+
+
+import com.example.articles.di.DaggerArticleAppComponent
+import com.facebook.drawee.backends.pipeline.Fresco
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class ArticlesApp : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerArticleAppComponent.builder().application(this).build()
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this);
+    }
+
+
+}
