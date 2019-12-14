abf0703d362654094ced154512cfa54518849be0
M MADHU KALYAN <raydriiger@gmail.com>
2019-12-14 10:54:25 +0300

    Ui fixes  Made changes in the item view to remove empty spaces

--- app/src/main/java/com/example/article/articles/data/models/Article.kt
+++ app/src/main/java/com/example/article/articles/data/models/Article.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class Article(
     @SerializedName("title")
-    val title: String,
+    val title: String?,
     @SerializedName("description")
-    val description: String,
+    val description: String?,
     @SerializedName("imageHref")
-    val imageHref: String
+    val imageHref: String?
 )
