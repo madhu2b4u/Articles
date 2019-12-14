46defce33434dee2af20b1d8dc759e72cc2c416a
M MADHU KALYAN <raydriiger@gmail.com>
2019-12-14 23:16:00 +0300

    Ui tests
    Tests network connectivity
    If list of rows is empty display no data layout

--- app/src/androidTest/java/com/example/article/articles/presentation/ui/ArticlesTest.kt
+++ app/src/androidTest/java/com/example/article/articles/presentation/ui/ArticlesTest.kt
@@ -1,9 +1,13 @@
 package com.example.article.articles.presentation.ui
 
 
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
@@ -11,17 +15,19 @@ import com.example.article.MainActivity
 import com.example.article.R
 import com.example.article.articles.presentation.ui.CustomAssertions.Companion.hasItemCount
 import com.example.article.articles.presentation.ui.CustomMatchers.Companion.withItemCount
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
-public class ArticlesTest {
+class ArticlesTest {
 
 
     @get:Rule
-    public val activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+    val activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(
         MainActivity::class.java,
         true,
         true
@@ -40,6 +46,30 @@ public class ArticlesTest {
     @Test
     fun testToSwipeUpToCheckCompleteDataLoaded() {
         onView(withId(R.id.articlesRecycler)).perform(ViewActions.swipeUp())
+    }
+
+    @Test
+    fun checkIfNoDataIsAvailable() {
+        if (getListCount() < 0) {
+            onView(withId(R.id.llNoDataLayout)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun testNetworkConnectivity() {
+        assertTrue(isConnected(activityRule.activity))
+    }
+
+    private fun getListCount(): Int {
+        val recyclerView =
+            activityRule.activity.findViewById(R.id.articlesRecycler) as RecyclerView
+        return recyclerView.adapter!!.itemCount
+    }
+    private fun isConnected(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
 }
